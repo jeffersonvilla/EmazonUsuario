49754ea68bc850919ae60eec50264235f50c1e03
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-09-03 14:26:50 -0500

    Inicio del proyecto

---
+++ src/test/java/com/jeffersonvilla/emazon/usuario/EmazonUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffersonvilla.emazon.usuario;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class EmazonUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertNull(null);
+	}
+
+}
